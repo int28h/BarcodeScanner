08e722bab4a749440fbeb4731b537376a20db969
Elena Listratova <listratova.elena.sergeevna@gmail.com>
2018-07-25 20:54:24 +0400

    init

---
+++ app/src/main/java/comint28h/github/codabarscanner/MainActivity.java
@@ -0,0 +1,81 @@
+package comint28h.github.codabarscanner;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    BarcodeDetector barcodeDetector;
+    CameraSource cameraSource;
+    SurfaceView cameraView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cameraView = findViewById(R.id.camera_view);
+        findViewById(R.id.button).setOnClickListener(MainActivity.this);
+
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.CODABAR).build();
+
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setFacing(CameraSource.CAMERA_FACING_BACK)
+                .setAutoFocusEnabled(true).build();
+
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void surfaceCreated(SurfaceHolder surfaceHolder) {
+                try{
+                    cameraSource.start(cameraView.getHolder());
+                } catch(IOException e) {
+                    Log.e("CAMERA SOURCE", e.getMessage());
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+
+                if(barcodes.size() != 0){
+                    Log.e("РАСПОЗНАНО", barcodes.get(0).displayValue.toString());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
